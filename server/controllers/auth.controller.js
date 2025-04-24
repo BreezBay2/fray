829158420b2108c67e6099fe9ab6aa87a97f81bf
Taro Altricher <altrichter.taro@outlook.com>
2025-04-24 19:38:57 +0200

    get currently authenticated user function

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -113,3 +113,13 @@ export const logout = async (req, res) => {
         res.status(500).json({ error: "Internal Server Error." });
     }
 };
+
+export const getCurrentUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id).select("-password");
+        res.status(200).json(user);
+    } catch (error) {
+        console.log("Error in Get Current User Controller", error.message);
+        res.status(500).json({ error: "Internal Server Error." });
+    }
+};
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import {
+    getCurrentUser,
+    login,
+    logout,
+    signup,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
+router.get("/user", protectRoute, getCurrentUser);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
