376558666e7c8e0b7f0e1cfb794dd00c467b6d63
Taro Altricher <altrichter.taro@outlook.com>
2025-04-24 18:59:45 +0200

    login functionality

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -71,3 +71,35 @@ export const signup = async (req, res) => {
         res.status(500).json({ error: "Internal Server Error." });
     }
 };
+
+export const login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        const passwordCheck = await bcrypt.compare(
+            password,
+            user?.password || ""
+        );
+
+        if (!user || !passwordCheck) {
+            return res
+                .status(400)
+                .json({ error: "Invalid username or password." });
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            username: user.username,
+            fullname: user.fullname,
+            email: user.email,
+            followers: user.followers,
+            following: user.following,
+            profileImg: user.profileImg,
+        });
+    } catch (error) {
+        console.log("Error in Login Controller.", error.message);
+        res.status(500).json({ error: "Internal Server Error." });
+    }
+};
