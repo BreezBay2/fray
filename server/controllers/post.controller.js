f7368ad49ddca4bfb6ca226fd23dd7f4062491e2
Taro Altricher <altrichter.taro@outlook.com>
2025-04-24 20:14:03 +0200

    Delete Post functionality

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -31,3 +31,24 @@ export const createPost = async (req, res) => {
         res.status(500).json({ error: "Internal Server Error." });
     }
 };
+
+export const deletePost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+            return res.status(404).json({ error: "Post not found." });
+        }
+
+        if (post.user.toString() !== req.user._id.toString()) {
+            return res
+                .status(401)
+                .json({ error: "You are not authorized to delete this post" });
+        }
+
+        await Post.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: "Post deleted successfully." });
+    } catch (error) {
+        console.log("Error in Delete Post Controller.", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
