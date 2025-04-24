7db60f629892aa4483a1bffcf680b790715949f2
Taro Altricher <altrichter.taro@outlook.com>
2025-04-24 14:55:46 +0200

    MongoDB setup

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
     res.send("Server ready");
 });
 
-app.listen(8000, () => {
-    "Server is running on PORT 8000";
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectMongoDB();
 });
