c44fb6e17fe4c324cbbeb3d3d3fc377d00af4c35
Taro Altricher <altrichter.taro@outlook.com>
2025-04-25 13:51:18 +0200

    like post functionality

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -81,6 +81,48 @@ export const commentOnPost = async (req, res) => {
     }
 };
 
+export const likeUnlikePost = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const { id: postId } = req.params;
+
+        const post = await Post.findById(postId);
+
+        if (!post) {
+            return res.status(404).json({ error: "Post not found." });
+        }
+
+        const userLikedPost = post.likes.includes(userId);
+
+        if (!userLikedPost) {
+            post.likes.push(userId);
+            await User.updateOne(
+                { _id: userId },
+                { $push: { likedPosts: postId } }
+            );
+
+            await post.save();
+
+            const updatedLikes = post.likes;
+            res.status(200).json(updatedLikes);
+        } else {
+            await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+            await User.updateOne(
+                { _id: userId },
+                { $pull: { likedPosts: postId } }
+            );
+
+            const updatedLikes = post.likes.filter(
+                (id) => id.toString() !== userId.toString()
+            );
+            res.status(200).json(updatedLikes);
+        }
+    } catch (error) {
+        console.log("Error in Like Unlike Controller.", error);
+        res.status(500).json({ error: "Internal Server Error." });
+    }
+};
+
 export const getAllPosts = async (req, res) => {
     try {
         const posts = await Post.find()
--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -6,13 +6,16 @@ import {
     deletePost,
     getAllPosts,
     getUserPosts,
+    likeUnlikePost,
 } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", protectRoute, createPost);
-router.delete("/:id", protectRoute, deletePost);
 router.post("/comment/:id", protectRoute, commentOnPost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+
+router.delete("/:id", protectRoute, deletePost);
 
 router.get("/all", protectRoute, getAllPosts);
 router.get("/user/:username", protectRoute, getUserPosts);
