96b958bcc7bfa90c6f359befe447d06df7f967c6
Taro Altricher <altrichter.taro@outlook.com>
2025-04-27 01:04:48 +0200

    like post functionality in frontend

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -2,14 +2,52 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FaRegHeart, FaRegComment, FaTrash } from "react-icons/fa";
 import "../styles/Post.css";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { formatPostDate } from "../utils/dateFormatter";
 
 const Post = ({ post }) => {
     const { data: authUser } = useQuery({ queryKey: ["authUser"] });
+    const queryClient = useQueryClient();
     const postUser = post.user;
     const isMyPost = authUser._id === post.user._id;
+    const isLiked = post.likes.includes(authUser._id);
     const formattedDate = formatPostDate(post.createdAt);
+
+    const { mutate: likePost, isPending: isLiking } = useMutation({
+        mutationFn: async () => {
+            try {
+                const res = await fetch(`/api/posts/like/${post._id}`, {
+                    method: "POST",
+                });
+
+                const data = await res.json();
+
+                if (!res.ok) {
+                    throw new Error(data.error || "Something went wrong.");
+                }
+                return data;
+            } catch (error) {
+                throw new Error(error);
+            }
+        },
+        onSuccess: (updatedLikes) => {
+            queryClient.setQueryData(["posts"], (oldData) => {
+                return oldData.map((p) => {
+                    if (p._id === post._id) {
+                        return { ...p, likes: updatedLikes };
+                    }
+                    return p;
+                });
+            });
+        },
+    });
+
+    const handleLikePost = () => {
+        if (isLiking) {
+            return;
+        }
+        likePost();
+    };
 
     return (
         <div className="post-container">
@@ -38,7 +76,10 @@ const Post = ({ post }) => {
                 <p>{post.text}</p>
             </div>
             <div className="post-interactions">
-                <div className="like-container">
+                <div
+                    className={isLiked ? "liked-container" : "like-container"}
+                    onClick={handleLikePost}
+                >
                     <FaRegHeart />
                     <p>{post.likes.length}</p>
                 </div>
